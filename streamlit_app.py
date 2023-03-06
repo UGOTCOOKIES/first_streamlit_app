4a96f21d8b4661afc6b40ff6dc4ae228477d7c18
UGOTCOOKIES <88298115+UGOTCOOKIES@users.noreply.github.com>
2023-03-06 14:22:56 +1300

    Update streamlit_app.py
    Pre-populate the picker list with Avocados and Strawberries

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Add a pick list so users can pick which fruits to add
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 #Display the table on the page
 st.dataframe(my_fruit_list)
