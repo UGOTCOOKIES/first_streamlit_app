6226439e2406e56cc7824deb9a7690178ccadb72
UGOTCOOKIES <88298115+UGOTCOOKIES@users.noreply.github.com>
2023-03-08 11:06:51 +1300

    Update streamlit_app.py
    Added last line which will not work due to control of flow concepts

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,6 @@ st.dataframe(my_data_rows)
 add_my_fruit = st.text_input('What fruit would you like to add?', 'jackfruit')
 st.write('Thanks for adding ', add_my_fruit)
 
+#This will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
